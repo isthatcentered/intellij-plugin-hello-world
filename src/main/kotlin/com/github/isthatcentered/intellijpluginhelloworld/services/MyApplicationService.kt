23b291f9bc5936e56ebc2e5689985969eb7b1e7f
GitHub Action <action@github.com>
2020-12-05 14:50:57 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/isthatcentered/intellijpluginhelloworld/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.isthatcentered.intellijpluginhelloworld.services
+
+import com.github.isthatcentered.intellijpluginhelloworld.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
